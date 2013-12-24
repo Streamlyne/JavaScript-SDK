f95c4190471f9caf3bfb7c5c275d6ca7a2c25ff0
Glavin Wiechert <Glavin.Wiechert@gmail.com>
2013-12-24 05:10:12 -0400

    Closes #1. Bower is all setup. Closes #2. Grunt all setup.

---
+++ Gruntfile.js
@@ -0,0 +1,108 @@
+/*
+Dawson Reid
+Glavin Wiechert
+*/
+
+module.exports = function(grunt) {
+
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // server
+        express: {
+            all: {
+                options: {
+                    port: 8123, 
+                    hostname: '0.0.0.0',
+                    bases: [ "dist", "tests", "bower_components" ],
+                    livereload: true
+                }
+            }
+        },
+        open: {
+            all: {
+                path: 'http://localhost:8123/docs/index.html'
+            }
+        },
+
+        // Compile 
+        uglify : {
+            compile: {
+                files: {
+                    'dist/streamlyne.min.js': [ 'src/streamlyne.js' ]
+                }
+            }
+        },
+
+        // Documentation
+        jsdoc: {
+            dist: {
+                src: [
+                    "src/*.js"
+                    , "README.md"
+                ],
+                options: {
+                    destination: 'dist/docs/'
+                    , recurse: true
+                    , tutorials: 'tutorials'
+                }
+            }
+        },
+
+        // file watchers
+        watch: {
+            options: {
+                livereload: true
+            },
+            js: {
+                files: [
+                    'src/*.js'
+                ],
+                tasks: [
+                    'uglify:compile',
+                    'jsdoc:dist'
+                ]
+            },
+
+            // server reload
+            server: {
+                options: {
+                    nospawn: true
+                },
+                files: [
+                    'src/*.js'
+                    ,'tests/test.html'
+                    ,'tutorials/*'
+                    
+                ], 
+                tasks: [
+                    'express'
+                ]
+            }
+
+        }
+  });
+
+  grunt.registerTask('compile', [
+    'uglify:compile',
+    'jsdoc:dist'
+  ]);
+
+  // Default task(s).
+  grunt.registerTask('server', [
+    'express',
+    'open',
+    'watch'
+  ]);
+  grunt.registerTask('default', [
+    'compile',
+    'server'
+  ]);
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  
+};
+
